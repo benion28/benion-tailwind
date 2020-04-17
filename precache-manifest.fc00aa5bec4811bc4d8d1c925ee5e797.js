b389eba41b4a628919ecc88100e2a24d0aac56bf
Bernard Iorver <bernard.benion.iorver@hotmail.com>
2020-04-17 02:28:28 +0100

    Updates

---
+++ precache-manifest.fc00aa5bec4811bc4d8d1c925ee5e797.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0e3c86b040a83cb7e017b28ac5215ec",
+    "url": "/benion-tailwind/index.html"
+  },
+  {
+    "revision": "3e2b6b931411e36bf83c",
+    "url": "/benion-tailwind/static/css/main.dfe6d384.chunk.css"
+  },
+  {
+    "revision": "7066909fe8e2a511eb7f",
+    "url": "/benion-tailwind/static/js/2.49819ef9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/benion-tailwind/static/js/2.49819ef9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e2b6b931411e36bf83c",
+    "url": "/benion-tailwind/static/js/main.f5f23c46.chunk.js"
+  },
+  {
+    "revision": "2707c5866b1b659ffc69",
+    "url": "/benion-tailwind/static/js/runtime-main.dae7be90.js"
+  }
+]);
